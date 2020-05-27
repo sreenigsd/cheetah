1e46a341237f37eb1b33af30bd1081cb7cd75d7d
Sreenidhi GSD <sreenigsd@hotmail.com>
2020-05-27 15:57:09 -0400

    Updated Runner

--- Playground/src/test/java/com/gsd/sreenidhi/automation/app/stepDefinitions/Runner.java
+++ Playground/src/test/java/com/gsd/sreenidhi/automation/app/stepDefinitions/Runner.java
@@ -1,16 +1,23 @@
 package com.gsd.sreenidhi.automation.app.stepDefinitions;
 
+import java.io.IOException;
+
 import com.gsd.sreenidhi.cheetah.engine.CheetahEngine;
+import com.gsd.sreenidhi.cheetah.engine.CucumberFormatter;
+import com.gsd.sreenidhi.cheetah.exception.CheetahException;
 
 import io.cucumber.java.Scenario;
+import io.cucumber.plugin.event.TestStepStarted;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
 
 public class Runner extends CheetahEngine{
 	
 	@Before
-    public void prepareScenario() throws Exception{
-		CheetahEngine.generateBase("ABC");
+    public void prepareScenario(io.cucumber.java.Scenario scenarioImpl) throws Exception{
+		CheetahEngine.generateBase(scenarioImpl, "ABC");
     }
     
     @After
@@ -19,5 +26,15 @@ public class Runner extends CheetahEngine{
      */
     public void processExecution(io.cucumber.java.Scenario scenarioImpl) throws Exception {
     	CheetahEngine.processPostAction(scenarioImpl, "ABC");
+    }
+    
+    @AfterStep
+    public void processStep(io.cucumber.java.Scenario scenarioImpl) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, CheetahException, IOException {
+    	CheetahEngine.afterStep(scenarioImpl);
+    }
+    
+    @BeforeStep
+    public void beforeStep() throws CheetahException {
+    	
     }
 }
